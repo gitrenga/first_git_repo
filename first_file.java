ebc3aa05252ce55af6b98bd7164f0fc977011b9a
rengasamyvenkittaramanofficial@gmail.com <Rengasamy.Venkittaraman2@unisys.com>
2020-01-20 15:54:41 +0530

    master commit

--- first_file.java
+++ first_file.java
@@ -1,3 +1,10 @@
+<<<<<<< Updated upstream
 im in master
 
 master changes1
+||||||| constructed merge base
+im in slave2
+ slave2 changed 2.02
+=======
+changes made by slave2 in 3.46pm
+>>>>>>> Stashed changes
